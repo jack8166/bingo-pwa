cd9267ddc80639b1bfca9acdcb46dc5e277b1cb6
jack8166 <gs8166@gmail.com>
2025-04-03 21:11:32 +0800

    Update [date].js

--- api/bingo/[date].js
+++ api/bingo/[date].js
@@ -3,8 +3,13 @@ export default async function handler(req, res) {
   const targetUrl = `https://lotto.auzonet.com/bingobingo/list_${date}.html`;
 
   try {
-    const response = await fetch(targetUrl);
+    const response = await fetch(targetUrl, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Mobile; iOS 17.4) AppleWebKit',
+      }
+    });
     const html = await response.text();
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate');
     res.status(200).send(html);
   } catch (error) {
